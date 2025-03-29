fc7cedda09ae7f6146a4caa867f1a067f6d6646a
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-03-29 23:43:15 +0530

    store

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterSlice from './counter/counterSlice'
+import notesSlice from './features/notes/notesSlice'
 
 export default configureStore({
   reducer: {
     counter: counterSlice,
+    notes: notesSlice,
   },
 })
